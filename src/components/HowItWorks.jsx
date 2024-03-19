2f5f33268c11b8e46e8c90859c3c0d7737716018
Derek Prieur <DPrieur123@gmail.com>
2024-03-19 10:07:46 -0400

    update

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -81,13 +81,13 @@ const HowItWorks = () => {
                                 best graphic performance by far
                             </span>.
                         </p>
+                        <p className="hiw-text g_fadeIn">
+                            Mobile {' '}
+                            <span className="text-white">
+                                games will look and feel so immersive
+                            </span>, with incredibly detailed environments and characters.
+                        </p>
                     </div>
-                    <p className="hiw-text g_fadeIn">
-                        Mobile {' '}
-                        <span className="text-white">
-                            games will look and feel so immersive
-                        </span>, with incredibly detailed environments and characters.
-                    </p>
                     <div className="flex-1 flex justify-center flex-col g_fadeIn">
                         <p className="hiw-text">New</p>
                         <p className="hiw-bigtext">Pro-class GPU</p>
